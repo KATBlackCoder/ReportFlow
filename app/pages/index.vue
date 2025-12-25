2163df2fea352311a9a228ce3111be1510f5d760
KATBlackCoder <isakedark5@gmail.com>
2025-12-25 15:12:56 +0000

    Refactor index.vue to enhance UI with a counter feature; added reset, increment, and decrement buttons for interactive functionality.

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,10 +1,26 @@
 <template>
-  <div>
-    <h1>Hello World</h1>
-    <UButton>Click me</UButton>
-  </div>
+  <UContainer>
+    <h1 class="text-2xl font-bold">Hello World</h1>
+    <p>{{ count }}</p>
+    <UButton @click="reset">Reset</UButton>
+    <UButton @click="increment">Increment</UButton>
+    <UButton @click="decrement">Decrement</UButton>
+  </UContainer>
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
 
-<style></style>
+  const count = ref(0);
+
+  function reset() {
+    count.value = 0;
+  }
+
+  function increment() {
+    count.value++;
+  }
+
+  function decrement() {
+    count.value--;
+  }
+</script>
